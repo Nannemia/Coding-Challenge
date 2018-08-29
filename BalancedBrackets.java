12b23989b3199fa2977b73b76d7ff9e1060d37f1
Nannemia <Nannemia@users.noreply.github.com>
2018-08-29 12:33:24 -0700

    Update BalancedBrackets.java

--- BalancedBrackets.java
+++ BalancedBrackets.java
@@ -1,57 +1,39 @@
 
 //Find if the brackets are in order, nested and closed properly
 public class BalancedBrackets {
-	public static boolean isBalanced(String expression) {
-        int brk=0,cur=0,sqr=0;
-        
-        StringBuilder tracker = new StringBuilder(); // Simulates the stack behavior to track which closing bracket is allowed
-        
-        for(int i=0; i<expression.length(); i++) {
-            char c = expression.charAt(i);
-            if(c == '(') {
-                tracker.append(')');
-                brk +=1;
-            } else if(c == '{') {
-                tracker.append('}');
-                cur +=1;
-            } else if(c == '[') {
-                tracker.append(']');
-                sqr +=1;
-            }
-            
-            Character allowedclosingBrk = (tracker.length() == 0) ? null:tracker.charAt(tracker.length()-1);
-            
-            if(c == ')') {
-                if (allowedclosingBrk != null && allowedclosingBrk == ')') {
-                    tracker.deleteCharAt(tracker.length()-1);
-                    brk -= 1;
-                } else {
-                    return false;
-                }
-            } else if(c == '}') {
-                if( allowedclosingBrk != null && allowedclosingBrk == '}') {
-                    tracker.deleteCharAt(tracker.length()-1);
-                    cur -= 1;
-                } else {
-                    return false;
-                }
-            } else if(c == ']') {
-                if( allowedclosingBrk != null && allowedclosingBrk == ']') {
-                    tracker.deleteCharAt(tracker.length()-1);
-                    sqr -= 1;
-                } else {
-                return false;
-                }
-            }
-        }
-        
-        
-        if (brk == 0 && cur == 0 && sqr == 0) {
-            return true;
-        }
-        return false;
-        
-    }
+	public static boolean isBalanced(String s) {
+		Stack<Character> c = new Stack<>();
+
+		for (int i = 0; i < s.length(); i++) {
+			Character ch = s.charAt(i);
+			switch (ch) {
+				case '{' :
+				case '[' :
+				case '(' :
+					c.push(ch);
+					break;
+				case '}' :
+					if (c.isEmpty() || c.pop() != '{')
+						return false;
+					break;
+				case ')' :
+					if (c.isEmpty() || c.pop() != '(')
+						return false;
+					break;
+
+				case ']' :
+					if (c.isEmpty() || c.pop() != '[')
+						return false;
+					break;
+				default :
+					return false;
+			}
+		}
+		if (c.isEmpty())
+			return true;
+		else
+			return false;
+	}
   
     public static void main(String[] args) {
 
